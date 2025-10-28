36100b832a37ccb5a580829035e7f17dbdf109d3
Mario González <mario@mariogl.com>
2025-10-28 12:58:36 +0100

    chore: start project with Vite and ESLint

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "no-console": "error",
+      "no-duplicate-imports": "error",
+    },
+  },
+];
