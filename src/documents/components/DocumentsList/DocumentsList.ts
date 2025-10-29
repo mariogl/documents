941321ffd0cd2ea3326e98c40e62ac9ab4df5ee0
Mario González <mario@mariogl.com>
2025-10-29 06:29:37 +0100

    feat(documents): improve semantic and styles of the documents list

--- src/documents/components/DocumentsList/DocumentsList.ts
+++ src/documents/components/DocumentsList/DocumentsList.ts
@@ -1,6 +1,8 @@
 import Component from "../../../components/Component";
+import HeadingComponent from "../../../components/Heading/Heading";
 import type { ComponentProps } from "../../../components/types";
 import type { DocumentViewModel } from "../../viewModel/types";
+import styles from "./DocumentsList.module.css";
 
 type DocumentsProps = {
   documents: DocumentViewModel[];
@@ -13,43 +15,78 @@ class DocumentsList extends Component<DocumentsProps> {
 
   render(): void {
     const container = document.createElement("table");
+    container.className = styles.documents;
+
     const thead = document.createElement("thead");
+    thead.className = styles.documents__head;
+
     const tbody = document.createElement("tbody");
 
     const headerRow = document.createElement("tr");
 
     const nameHeader = document.createElement("th");
+    nameHeader.className = styles.documents__header;
     nameHeader.textContent = "Name";
     headerRow.appendChild(nameHeader);
 
     const contributorsHeader = document.createElement("th");
+    contributorsHeader.className = styles.documents__header;
     contributorsHeader.textContent = "Contributors";
     headerRow.appendChild(contributorsHeader);
 
     const attachmentsHeader = document.createElement("th");
+    attachmentsHeader.className = styles.documents__header;
     attachmentsHeader.textContent = "Attachments";
     headerRow.appendChild(attachmentsHeader);
 
     thead.appendChild(headerRow);
     container.appendChild(thead);
 
-    this.props.documents.forEach(({ name, contributors, attachments }) => {
-      const row = document.createElement("tr");
+    this.props.documents.forEach(
+      ({ name, contributors, attachments, version }) => {
+        const row = document.createElement("tr");
+        row.className = styles.documents__row;
 
-      const nameCell = document.createElement("td");
-      nameCell.textContent = name;
-      row.appendChild(nameCell);
+        const nameCell = document.createElement("td");
+        nameCell.className = styles.documents__cell;
+        const documentNameHeading = new HeadingComponent({
+          level: 2,
+          text: name,
+          className: styles.documents__name,
+        });
+        nameCell.appendChild(documentNameHeading.getElement());
+        const documentVersion = document.createElement("span");
+        documentVersion.textContent = `Version: ${version}`;
+        nameCell.appendChild(documentVersion);
+        row.appendChild(nameCell);
 
-      const contributorsCell = document.createElement("td");
-      contributorsCell.textContent = contributors.join(", ");
-      row.appendChild(contributorsCell);
+        const contributorsCell = document.createElement("td");
+        contributorsCell.className = styles.documents__cell;
+        const contributorsList = document.createElement("ul");
+        contributorsList.className = styles.documents__list;
+        contributors.forEach((contributor) => {
+          const listItem = document.createElement("li");
+          listItem.textContent = contributor;
+          contributorsList.appendChild(listItem);
+        });
+        contributorsCell.appendChild(contributorsList);
+        row.appendChild(contributorsCell);
 
-      const attachmentsCell = document.createElement("td");
-      attachmentsCell.textContent = attachments.join(", ");
-      row.appendChild(attachmentsCell);
+        const attachmentsCell = document.createElement("td");
+        attachmentsCell.className = styles.documents__cell;
+        const attachmentsList = document.createElement("ul");
+        attachmentsList.className = styles.documents__list;
+        attachments.forEach((attachment) => {
+          const listItem = document.createElement("li");
+          listItem.textContent = attachment;
+          attachmentsList.appendChild(listItem);
+        });
+        attachmentsCell.appendChild(attachmentsList);
+        row.appendChild(attachmentsCell);
 
-      tbody.appendChild(row);
-    });
+        tbody.appendChild(row);
+      },
+    );
 
     container.appendChild(tbody);
 
