d90c6a8934eb6f3216251422a79d2999b12fb308
Mario González <mario@mariogl.com>
2025-10-30 23:00:23 +0100

    feat(documents): add version input to new document's form

--- src/documents/components/NewDocumentForm/NewDocumentForm.ts
+++ src/documents/components/NewDocumentForm/NewDocumentForm.ts
@@ -1,3 +1,4 @@
+import ButtonComponent from "../../../shared/components/Button/Button";
 import Component from "../../../shared/components/Component";
 import TextboxComponent from "../../../shared/components/Textbox/Textbox";
 
@@ -6,7 +7,10 @@ import styles from "./NewDocumentForm.module.css";
 class NewDocumentFormComponent extends Component {
   protected render(): Element {
     const form = this.createForm();
-    form.appendChild(this.createTextbox());
+
+    form.appendChild(this.createTextbox("name", "Name:"));
+    form.appendChild(this.createTextbox("version", "Version:"));
+    form.appendChild(this.createSubmitButton());
 
     return form;
   }
@@ -17,14 +21,23 @@ class NewDocumentFormComponent extends Component {
     return form;
   }
 
-  private createTextbox() {
+  private createTextbox(id: string, label: string) {
     const textbox = new TextboxComponent({
-      id: "name",
-      label: "Document Name:",
+      id,
+      label,
       type: "text",
       required: true,
     });
     return textbox.getElement();
+  }
+
+  private createSubmitButton() {
+    const button = new ButtonComponent({
+      text: "Create Document",
+      size: "auto",
+      type: "submit",
+    });
+    return button.getElement();
   }
 }
 
